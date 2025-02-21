90d402dece323ef5dcb0657ad71977c75d053b6e
Sheikh Muhammad Antor <sheikhmuhammadantor@gmail.com>
2025-02-21 11:25:03 +0600

    feat: add AddTask component and integrate it into Home page layout.

--- ZenTaskClient/src/page/home/Home.jsx
+++ ZenTaskClient/src/page/home/Home.jsx
@@ -1,16 +1,22 @@
 import Footer from "../../components/layout/Footer";
 import Header from "../../components/layout/Header";
+import AddTask from "../../components/task/AddTask";
 
 function Home() {
 
-    return (
-      <section>
-        <Header />
+  return (
+    <section className="container mx-auto">
+      <Header />
+
+
+      <section className="min-h-[70vh]">
+        <AddTask />
+      </section>
+
 
       <Footer />
-      </section>
-    )
-  }
-  
-  export default Home;
-  
+    </section>
+  )
+}
+
+export default Home;
